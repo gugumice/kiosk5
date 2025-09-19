a9486bd830491f8dae30248a33c495d61b508520
Gunars Frickauss <gunarsf@gmail.com>
2025-09-19 16:39:11 +0300

    Update initkio.sh

--- initkio.sh
+++ initkio.sh
@@ -8,8 +8,11 @@ ipo=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1 |  cut -d. -f2);
 newHostname="rapi-kio6-"$ipo
 
 hostnamectl set-hostname ${newHostname} --static
-printf ${newHostname} > /etc/hostname
+# printf ${newHostname} > /etc/hostname
 sed -i '/^127.0.0.1/s/.*/127.0.0.1\t'${newHostname}'/g' /etc/hosts
+
+
+
 sed -i '/^#NTP=.*/a FallbackNTP=laiks.egl.local' /etc/systemd/timesyncd.conf
 printf '10.100.20.104   laiks.egl.local\n' >> /etc/hosts
 printf '10.100.50.102   cache.egl.local\n' >> /etc/hosts
