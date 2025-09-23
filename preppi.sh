caa71925f6cf2deffcd4f112ec59004f0f5c6a3c
Gunars Frickauss <gunarsf@gmail.com>
2025-09-23 11:27:59 +0300

    Update preppi.sh

--- preppi.sh
+++ preppi.sh
@@ -10,7 +10,7 @@ apt-get update && apt-get upgrade -y
 systemctl disable bluetooth.service
 systemctl disable hciuart.service
 
-printf "Install tkinter & stuff"
+printf "Install tkinter & stuff\n"
 sudo apt-get install xserver-xorg -y
 sudo apt-get install xinit -y
 sudo apt-get install x11-xserver-utils -y
@@ -24,30 +24,30 @@ ln /opt/kiosk/firstboot.service /lib/systemd/system/firstboot.service
 ln /opt/kiosk/kiosk.ini /home/pi/kiosk.ini
 systemctl enable firstboot.service
 
-printf "Setting timezone & logs"
+printf "Setting timezone & logs\n"
 timedatectl set-timezone Europe/Riga
-printf "Setting logfiles" 
+printf "Setting logfiles\n" 
 ./make_logdirs.sh "/var/log/kiosk/kiosk.log"
 ln -s "${file_path}" /home/pi/kiosk.log
 
-printf "Updating config.sys"
+printf "Updating config.sys\n"
 ./update_config.sh "/boot/firmware/config.txt"
 
-printf "Config watchdog"
+printf "Config watchdog\n"
 addgroup watchdog
 usermod -a -G watchdog "${USER}"
-printf'KERNEL=="watchdog", MODE="0660", GROUP="watchdog"' > /etc/udev/rules.d/60-watchdog.rules 
+printf'KERNEL=="watchdog", MODE="0660", GROUP="watchdog"\n' > /etc/udev/rules.d/60-watchdog.rules 
 
-printf "Setting touchoad"
+printf "Setting touchoad\n"
 ./touchpad_rules.sh
 
-printf "Setting PIP"
+printf "Setting PIP\n"
 apt-get --yes install python3-pi
 ####
 apt-get install python3-pil.imagetk
 sed -i '/^\[global\]$/a break-system-packages = true' /etc/pip.conf
 
-printf "Install & configure CUPS"
+printf "Install & configure CUPS\n"
 
 apt-get --yes install libcups2-dev cups cups-bsd -y
 sleep 1
@@ -59,16 +59,16 @@ usermod -aG lp $USER
 sleep 1
 service cups restart
 
-printf "Installing venv"
+printf "Installing venv\n"
 
-pip3 install pyserial
 sudo apt install python3-venv -y
 python3 -m venv --system-site-packages "${WORK_DIR}.venv"
 source .venv/bin/activate
 pip3 install customtkinter
 pip3 install pillow
+pip3 install pyserial
 
-printf "Install screen brightness utility"
+printf "Install screen brightness utility\n"
 cd "${USER_DIR}"
 
 wget https://files.waveshare.com/wiki/common/Brightness.zip
