0899164b727ab57d69e5661578df3708599093af
Gunars Frickauss <gunarsf@gmail.com>
2025-09-23 11:49:13 +0300

    Update initkio.sh

--- initkio.sh
+++ initkio.sh
@@ -6,7 +6,7 @@ printf "01 10 * * * sudo shutdown -r\n" >>  /var/spool/cron/crontabs/root;
 cp /etc/hosts /etc/hosts.backup;
 oldHostname=$(cat /proc/sys/kernel/hostname);
 ipo=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1 |  cut -d. -f2);
-newHostname="rapi-kio6-${ipo}"
+newHostname="rapi-kio5scr-${ipo}"
 
 hostnamectl set-hostname ${newHostname} --static
 # printf ${newHostname} > /etc/hostname
@@ -15,6 +15,5 @@ sed -i -e 's/'${oldHostname}'/'${newHostname}'/g' /etc/hosts;
 sed -i '/^#NTP=.*/a FallbackNTP=laiks.egl.local' /etc/systemd/timesyncd.conf;
 printf '10.100.20.104   laiks.egl.local\n' >> /etc/hosts;
 printf '10.100.50.102   cache.egl.local\n' >> /etc/hosts;
-
-printf "01 10 * * * sudo shutdown -r" >>  /var/spool/cron/crontabs/root;
+printf "01 10 * * * sudo shutdown -r\nn" >>  /var/spool/cron/crontabs/root;
 /sbin/shutdown -r now;
