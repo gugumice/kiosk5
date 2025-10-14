8f99e5e56204927277b6f5b76c52b48dde499196
Gunars Frickauss <gunarsf@gmail.com>
2025-10-14 16:35:53 +0300

    Update preppi.sh

--- preppi.sh
+++ preppi.sh
@@ -4,16 +4,14 @@ USER="pi"
 GROUP="kiosk"
 WORK_DIR="/opt/kiosk/"
 USER_DIR="/home/pi/"
-
-apt-get update && apt-get upgrade -y
 # Disable bluetooth & WiFi
 systemctl disable bluetooth.service
 systemctl disable hciuart.service
 
 printf "Install tkinter & stuff\n"
-apt-get install xserver-xorg -y
-apt-get install xinit -y
-apt-get install x11-xserver-utils -y
+# apt-get install xserver-xorg -y
+# apt-get install xinit -y
+# apt-get install x11-xserver-utils -y
 apt install raspberrypi-ui-mods -y
 apt-get install python3-tk -y
 printf "Changing Xwrapper.config\n"
@@ -61,7 +59,6 @@ sleep 1
 service cups restart
 
 printf "Installing venv\n"
-
 apt install python3-venv -y
 python3 -m venv --system-site-packages "${WORK_DIR}.venv"
 source .venv/bin/activate
@@ -79,5 +76,5 @@ unzip "${USER_DIR}Brightness.zip"
 chmod a+x "${USER_DIR}Brightness/install.sh"
 source "${USER_DIR}Brightness/install.sh"
 rm -r "${USER_DIR}Brightness"
-
+apt-get update && apt-get upgrade -y
 #/usr/sbin/shutdown -r now
